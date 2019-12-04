fbc5c8c10f80349ac4b375181119defb1b477458
Axel Axelsson <axel.axelsson10@gmail.com>
2019-12-04 16:37:09 +0100

    Initial commit for new React app

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/styles.scss";
+import "normalize.css/normalize.css";
+
+const MyJsx = () => <p>My jsx!</p>;
+
+ReactDOM.render(<MyJsx />, document.getElementById("app"));
